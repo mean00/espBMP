2b037503d57ab2febd494b39e308cc40b6b146ce
mean <fixounet@free.fr>
2022-03-03 14:17:12 +0100

    import

---
+++ run-esp32.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd build
+export dev=/dev/ttyACM0
+python $IDF_PATH/components/esptool_py/esptool/esptool.py -p $dev write_flash @flash_project_args
+python $IDF_PATH/tools/idf_monitor.py -p $dev idf_as_lib.elf

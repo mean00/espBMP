84177f53467f88f43e760a25c2d67a6244d312b2
mean <fixounet@free.fr>
2022-03-03 15:27:41 +0100

    connect to AP

--- script/run-esp32.sh
+++ script/run-esp32.sh
@@ -2,4 +2,4 @@
 cd build
 export dev=/dev/ttyACM0
 python $IDF_PATH/components/esptool_py/esptool/esptool.py -p $dev write_flash @flash_project_args
-python $IDF_PATH/tools/idf_monitor.py -p $dev idf_as_lib.elf
+python $IDF_PATH/tools/idf_monitor.py -p $dev espMagic.elf
